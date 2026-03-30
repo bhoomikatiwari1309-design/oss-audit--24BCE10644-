3dd41d232d4b381f6cc67d9a3df25eb6abba08b7
bhoomikatiwari1309-design <bhoomika.tiwari1309@gmail.com>
2026-03-31 00:21:34 +0530

    script3_disk_auditor.sh

--- scripts/script3_disk_auditor.sh
+++ scripts/script3_disk_auditor.sh
@@ -6,7 +6,7 @@ DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
 echo "Directory Audit Report"
 echo "----------------------"
-
+ 
 for DIR in "${DIRS[@]}"; do
     if [ -d "$DIR" ]; then
         PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
